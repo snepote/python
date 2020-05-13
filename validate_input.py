554b99193007d25b4842c484d690cbb12ab7df48
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-05-13 17:06:46 +0200

    code formatting

--- validate_input.py
+++ validate_input.py
@@ -12,21 +12,18 @@ def input_value(prompt, regex, default = None):
             break
     return value
 
-username_regex = re.compile("^[a-z]+\.[a-z]+$")
-domain_regex   = re.compile("^[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)$")
+username_regex  = re.compile("^[a-z]+\.[a-z]+$")
+domain_regex    = re.compile("^[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)$")
 
-source_user     = input_value("User login [firstname.lastname]: ", username_regex)
-source_domain   = input_value("User domain [helpling.com]: ", domain_regex, "helpling.com")
-redirect_user   = input_value("Redirect user login [firstname.lastname]: ", username_regex)
+source_user     = input_value("User login (format: firstname.lastname): ", username_regex)
+source_domain   = input_value("User domain [example.com]: ", domain_regex, "helpling.com")
+redirect_user   = input_value("Redirect user login (format: firstname.lastname): ", username_regex)
 redirect_domain = input_value("Redirect user domain [" + source_domain + "]: ", domain_regex, source_domain)
 
-print("User login is " + source_user)
-print("User domain is " + source_domain)
-print("Redirect user is " + redirect_user)
-print("Redirect user domain " + redirect_domain)
+print("We will redirect <%s@%s> to <%s@%s>" %(source_user, source_domain, redirect_user, redirect_domain))
 
 while True:
-    proceed = raw_input("Proceed (y/n)?")
+    proceed = raw_input("Proceed (y/n)? ")
     if proceed == "y":
         print("We proceed")
         break
