a758863011f2cf5b23f6c103e0b613557f83ccd1
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-05-14 14:09:22 +0200

    better string interpolation

--- validate_input.py
+++ validate_input.py
@@ -19,7 +19,7 @@ domain_regex    = re.compile("^[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\u
 source_user     = input_value("User login (format: firstname.lastname): ", username_regex)
 source_domain   = input_value("User domain [example.com]: ", domain_regex, "example.com")
 redirect_user   = input_value("Redirect user login (format: firstname.lastname): ", username_regex)
-redirect_domain = input_value("Redirect user domain [" + source_domain + "]: ", domain_regex, source_domain)
+redirect_domain = input_value("Redirect user domain [%s]: " %(source_domain), domain_regex, source_domain)
 
 print("We will redirect <%s@%s> to <%s@%s>" %(source_user, source_domain, redirect_user, redirect_domain))
 
