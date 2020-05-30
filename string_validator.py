6ae069123602f13fa118706d19548b1846754f13
Sebastian Nepote <sebastian.nepote@helpling.com>
2020-05-30 15:17:05 +0200

    simplified string validator and passes tests

--- string_validator.py
+++ string_validator.py
@@ -2,10 +2,12 @@
 import re
 
 username_regex  = re.compile(r'^[a-z]+\.[a-z]+$')
-domain_regex    = re.compile(r'^[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)$')
+domain_regex    = re.compile(r'^[a-z0-9]+\.?[a-z-0-9]+(\.[a-z]{2,})+$')
 
 def is_username(string):
     return False if not username_regex.match(string) else True
 
 def is_domain(string):
+    if len(string) > 253:
+        return False
     return False if not domain_regex.match(string) else True
--- tests/test_string_validator.py
+++ tests/test_string_validator.py
@@ -1,4 +1,5 @@
 import string_validator
+import pytest
 
 validate_string = string_validator
 
@@ -23,11 +24,18 @@ def test_valid_subdomain():
 def test_valid_domain_with_dash():
     assert validate_string.is_domain("some-example.com")
 
-def test_domain_ending_with_numbers():
+def test_domain_starting_with_non_char():
+    assert not validate_string.is_domain("-example.com")
+
+def test_domain_ending_with_non_char():
     assert not validate_string.is_domain("example.com@")
 
 def test_domain_without_protocol():
     assert not validate_string.is_domain("http://example.com")
 
-def test_domain_with_non_char():
-    assert not validate_string.is_domain("http://@example.com")
+def test_domain_is_email():
+    assert not validate_string.is_domain("user@example.com")
+
+def test_domain_length_is_less_or_equal_253():
+    long_domain = "nqnwsjlgxuywzojugeagcfbphrvgacffmuqppnehpzenoixzplrwmhisoytxfgsbarqgrcgqqblyhrdxfqzeirzojyrgjtbfapnzkguqzhyuirazmymxakypbwulmuxcjcfxkhgcgbivohcbxvvkgczaqdoekepwzcxkciwjdpkrdaqdywezqauskkfhmgjubmxhddcbqjvimeqfgjnwhxeufqamrlfxddmlkeonbwmwiypnknmqbclsgm.tlo"
+    assert not validate_string.is_domain(long_domain)
